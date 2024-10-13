f86626558b20ba021509d778e0d9a00f612074ae
WhiteCornFour <bapnhn1810@gmail.com>
2024-10-13 14:37:53 +0700

    Commit test

--- app/src/main/java/com/example/doanungdung2/View/MainActivity.java
+++ app/src/main/java/com/example/doanungdung2/View/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.doanungdung2.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.doanungdung2.R;
 
@@ -12,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Test", "Ngô Hoàng Nam");
     }
 }
