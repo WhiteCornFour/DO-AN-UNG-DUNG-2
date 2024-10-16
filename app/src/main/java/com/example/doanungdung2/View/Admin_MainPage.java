855cfd8f8f0ad0fd87a76cce3e20e6d2e30bdd08
WhiteCornFour <bapnhn1810@gmail.com>
2024-10-16 12:49:15 +0700

    Completed Admin Main Page V.0.1

---
+++ app/src/main/java/com/example/doanungdung2/View/Admin_MainPage.java
@@ -0,0 +1,68 @@
+package com.example.doanungdung2.View;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.doanungdung2.R;
+import com.google.android.material.navigation.NavigationView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Admin_MainPage extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    NavigationView navigationView;
+
+    TextView tvTenAD, tvTKAD;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_main_page);
+        addControl();
+
+        //----------------Gắn thông tin đăng nhập cho các text view bên header_layout
+        View headerView = navigationView.getHeaderView(0);
+        tvTenAD = headerView.findViewById(R.id.tvTenAD);
+        tvTKAD = headerView.findViewById(R.id.tvTKAD);
+
+        tvTenAD.setText("Ngô Hoàng Nam");
+        tvTKAD.setText("hoangnam@gmail.com");
+
+        setSupportActionBar(toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new
+                ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.open_nav, R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView = (NavigationView) findViewById(R.id.nav_drawer);
+        Admin_DanhMucQuanLy_Fragment fragment = new Admin_DanhMucQuanLy_Fragment();
+        replaceFragment(fragment);
+        drawerLayout.closeDrawer(GravityCompat.START);
+    }
+
+    void addControl()
+    {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_drawer);
+        tvTenAD = (TextView) findViewById(R.id.tvTenAD);
+        tvTKAD = (TextView) findViewById(R.id.tvTKAD);
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.content_frame, fragment);
+        fragmentTransaction.commit();
+    }
+}
