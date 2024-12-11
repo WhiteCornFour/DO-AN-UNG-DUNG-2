2805765acaa00ea5308b6a767fa477c8a67027b7
Linh02122004 <137763237+Linh02122004@users.noreply.github.com>
2024-12-11 18:08:53 +0700

    update first_login + choose_login + sentences

--- app/src/main/java/com/example/doanungdung2/View/User_First_Before_Login.java
+++ app/src/main/java/com/example/doanungdung2/View/User_First_Before_Login.java
@@ -1,24 +1,29 @@
 package com.example.doanungdung2.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.ColorUtils;
 
-import android.animation.ObjectAnimator;
+
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.doanungdung2.R;
 
 public class User_First_Before_Login extends AppCompatActivity {
 
-    ImageView monkeyImage;
-    TextView tvXinchao;
+    ConstraintLayout mainLayout;
+    TextView tvEasy, tvWelcome;
     Button btnNext;
 
     @Override
@@ -32,30 +37,37 @@ public class User_First_Before_Login extends AppCompatActivity {
 
     }
     void addControl() {
-        monkeyImage = findViewById(R.id.monkeyImage);
-        tvXinchao = findViewById(R.id.tvXinChao);
+        mainLayout = findViewById(R.id.mainLayout);
+        tvEasy = findViewById(R.id.tvEasy);
+        tvWelcome = findViewById(R.id.tvWelcome);
         btnNext = findViewById(R.id.btnNext);
     }
     void addEvent() {
-        Animation slideIn = AnimationUtils.loadAnimation(this, R.anim.text_slide_in);
-        slideIn.setRepeatCount(1);// lap lai
-        slideIn.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-                Animation shake = AnimationUtils.loadAnimation(User_First_Before_Login.this, R.anim.wave);
-                shake.setRepeatCount(1);
-                monkeyImage.startAnimation(shake);
-            }
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                monkeyImage.clearAnimation(); // Dừng hiệu ứng
-            }
+        ConstraintLayout constraintLayout = findViewById(R.id.mainLayout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
 
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-            }
-        });
-        tvXinchao.startAnimation(slideIn);
+//        Animation slideIn = AnimationUtils.loadAnimation(this, R.anim.text_slide_in);
+//        slideIn.setRepeatCount(1);// lap lai
+//        slideIn.setAnimationListener(new Animation.AnimationListener() {
+//            @Override
+//            public void onAnimationStart(Animation animation) {
+//                Animation shake = AnimationUtils.loadAnimation(User_First_Before_Login.this, 3R.anim.wave);
+//                shake.setRepeatCount(1);
+//                monkeyImage.startAnimation(shake);
+//            }
+//            @Override
+//            public void onAnimationEnd(Animation animation) {
+//                monkeyImage.clearAnimation(); // Dừng hiệu ứng
+//            }
+//
+//            @Override
+//            public void onAnimationRepeat(Animation animation) {
+//            }
+//        });
+//        tvXinchao.startAnimation(slideIn);
 
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
